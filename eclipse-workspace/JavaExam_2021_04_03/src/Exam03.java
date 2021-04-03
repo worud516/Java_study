8ab8338a4dfd54a849c7e78a5c6a5d113cb07bc9
worud516 <worud516@naver.com>
2021-04-03 17:51:05 +0900

    academy_review_0403

---
+++ eclipse-workspace/JavaExam_2021_04_03/src/Exam03.java
@@ -0,0 +1,31 @@
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
+
+class Exam03 {
+  public static void main(String[] args) {
+    int dan = 8;
+    
+    System.out.printf("%d * 1 = %d\n", dan, dan * 1);
+    System.out.printf("%d * 2 = %d\n", dan, dan * 2);
+    System.out.printf("%d * 3 = %d\n", dan, dan * 3);
+    System.out.printf("%d * 4 = %d\n", dan, dan * 4);
+    System.out.printf("%d * 5 = %d\n", dan, dan * 5);
+    System.out.printf("%d * 6 = %d\n", dan, dan * 6);
+    System.out.printf("%d * 7 = %d\n", dan, dan * 7);
+    System.out.printf("%d * 8 = %d\n", dan, dan * 8);
+    System.out.printf("%d * 9 = %d\n", dan, dan * 9);
+    
+    // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
+  }
+}
