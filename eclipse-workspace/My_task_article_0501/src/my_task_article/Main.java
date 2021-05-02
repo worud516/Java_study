a937eb7e05e50bc132ea43509e24c0f199ca6810
worud516 <worud516@naver.com>
2021-05-02 17:49:49 +0900

    article update

--- eclipse-workspace/My_task_article_0501/src/my_task_article/Main.java
+++ eclipse-workspace/My_task_article_0501/src/my_task_article/Main.java
@@ -50,7 +50,55 @@ public class Main {
 					
 					System.out.printf("%d   /   %s\n",article.id, article.title);
 				}
-			} else {
+			}else if(command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				
+				Article foundArticle = null;
+				
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if(article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2021-05-02 16:22:22\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				
+			}else if(command.startsWith("article delete ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				
+				int foundIndex = -1;
+				
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if(article.id == id) {
+						foundIndex = i;
+						break;
+					}
+				}
+				
+				if(foundIndex == -1) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				
+				articles.remove(foundIndex);
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
+				
+			}else {
 				System.out.printf("%s은(는) 존재하지 않는 명령어 입니다.\n", command);
 			}
 			
