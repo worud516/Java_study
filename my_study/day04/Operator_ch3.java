47fa515376c5d9c7b64ccf9e684c442b640ce8e0
worud516 <worud516@naver.com>
2021-06-18 15:24:24 +0900

    연산자 1

---
+++ my_study/day04/Operator_ch3.java
@@ -0,0 +1,19 @@
+package day04;
+
+public class Operator_ch3 {
+	public static void main(String[] args) {
+//			int a=1;
+//			System.out.println(a); //a=1
+//			
+//			a++; // a값 1증가 시켜라, 후위형
+//			System.out.println(a);//a=2 1증가됨!
+//			
+//			System.out.println(++a);//전위형: 증가먼저! 출력결과:3
+//			System.out.println(a++);//후위형:변수사용먼저! 출력결과:3
+//			System.out.println(a);//출력결과:4
+//			
+//			
+			System.out.println(~5);
+	}
+
+}
