2dd29aa73a72669715c58dd3ecf9ff71ca3f109e
worud516 <worud516@naver.com>
2021-06-19 12:46:04 +0900

    비트연산자

---
+++ my_study/day05/Xor_practice.java
@@ -0,0 +1,20 @@
+package day05;
+
+public class Xor_practice {
+	public static void main(String[] args) {
+			int a=15; //1111(2)
+			int b=5;  //0101(2)
+			//비트 논리연산자
+			System.out.println(a&b); //0101(2) = 5
+			System.out.println(a|b); //1111(2) =15
+			System.out.println(a^b); //1010(2) =10
+			
+			System.out.println(~b); // 1010(2) =10x -> -6
+
+			//비트 쉬프트 연산자
+			System.out.println(a>>2);
+			System.out.println(b<<4);
+			
+	}
+
+}
