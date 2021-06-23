0b99634596070f80e00391e91ad02604b9fe627e
worud516 <worud516@naver.com>
2021-06-24 00:42:58 +0900

    for문

---
+++ my_study/day09/For2.java
@@ -0,0 +1,20 @@
+package day09;
+
+public class For2 {
+	public static void main(String[] args) {
+			int sum=0;
+			//1~10까지의 합을 계산해서 출력
+			for(int i=1;i<=10;i++) {
+				sum=sum+i;
+			}
+			System.out.println(sum);
+//			sum=sum+1;//1
+//			sum=sum+2;//3
+//			sum=sum+3;//6
+//			sum=sum+4;//10
+//			//..
+//			sum=sum+10;
+			
+	}
+
+}
