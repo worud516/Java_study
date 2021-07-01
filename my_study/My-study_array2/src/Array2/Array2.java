83f8049914ee570bd7a93f01238f23d7e285e4a9
worud516 <worud516@naver.com>
2021-07-01 18:53:52 +0900

    2차원 배열

---
+++ my_study/My-study_array2/src/Array2/Array2.java
@@ -0,0 +1,15 @@
+package Array2;
+
+public class Array2 {
+	public static void main(String[] args) {
+		
+		// 5개의 정수형 변수를 묶는 1차원 배열 x2 = 2차원 배열
+		
+		int ar1[] = new int[5];
+		int ar2[] = new int[5];
+		
+		int ar3[][] = new int[2][5];
+		
+		
+	}
+}
---
+++ my_study/day14/Array2.java
@@ -0,0 +1,14 @@
+package day14;
+
+public class Array2 {
+	public static void main(String[] args) {
+			//5개의 정수형 변수 묶는 1차원배열 x2 = 2차원배열
+			int ar1[]=new int[5];
+			int ar2[]=new int[5];
+			
+			//자료형 배열명[][]=new int[묶음크기][변수갯수];
+			int ar3[][]=new int[2][5];
+			
+	}
+
+}
