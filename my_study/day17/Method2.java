f24b07ce22a89e7e0c2999dbf8ac98f15c4559a4
worud516 <worud516@naver.com>
2021-07-07 21:36:14 +0900

    day17 매서드

---
+++ my_study/day17/Method2.java
@@ -0,0 +1,12 @@
+package day17;
+
+public class Method2 {
+	public static void main(String[] args) {
+		System.out.println(f());
+	}
+	static int f(){
+		System.out.println("f() »£√‚!");
+		return 100;
+	}
+
+}
