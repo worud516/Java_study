9efe274d13423805624c5820bbb78f7d2c0ba92d
worud516 <worud516@naver.com>
2021-07-15 16:06:09 +0900

    Abstraction

---
+++ my_study/day26/AbstractClass1.java
@@ -0,0 +1,67 @@
+package day26;
+
+public class AbstractClass1 {
+	public static void main(String[] args) {
+		Pokemon pokemon= new Pikachu();
+		System.out.println("이 포켓몬은:"+pokemon.getName());
+		pokemon.attack();
+		pokemon.sound();
+		
+		pokemon=new Squirtle();
+		System.out.println("이 포켓몬은:"+pokemon.getName());
+		pokemon.attack();
+		pokemon.sound();
+		
+		
+	}
+}
+abstract class Pokemon{
+	String name;
+	abstract void attack();
+	abstract void sound();
+	public String getName() {
+		return this.name;
+	}
+}
+class Pikachu extends Pokemon{
+	Pikachu(){
+		this.name="피카츄";
+	}
+	@Override
+	void attack() {
+		System.out.println("전기 공격");
+	}
+	@Override
+	void sound() {
+		System.out.println("피카피카!");
+	}
+}
+
+class Squirtle extends Pokemon{
+	Squirtle(){
+		this.name="꼬부기";
+	}
+	@Override
+	void attack() {
+		System.out.println("물 공격");
+	}
+
+	@Override
+	void sound() {
+		System.out.println("꼬북 꼬북!");
+	}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
