a4821877193e7d62a25a9b90b19410b4c77d5847
worud516 <worud516@naver.com>
2021-07-30 14:51:12 +0900

    map

---
+++ my_study/day39/Map1.java
@@ -0,0 +1,27 @@
+package day39;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Map1 {
+	public static void main(String[] args) {
+		HashMap hm = new HashMap();
+		Scanner sc=  new Scanner(System.in);
+		
+		hm.put("apple","사과");
+		hm.put("paper","종이");
+		hm.put("flower","꽃");
+		String voca;
+		
+		System.out.print("알고 싶은 단어를 입력하세요:");
+		voca = sc.nextLine();
+		
+		if(hm.containsKey(voca)) {
+			System.out.println("해당하는 뜻은: "+hm.get(voca));
+		}
+		else {
+			System.out.println("해당 단어에 대한 뜻은 데이터베이스에 없습니다.");
+		}
+	}
+
+}
