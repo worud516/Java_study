8a60d56b6684177de051806a1796df88fd064153
worud516 <worud516@naver.com>
2021-08-06 21:08:35 +0900

    스레드 동기화 문제발생

---
+++ my_study/day44/ShareBank.java
@@ -0,0 +1,32 @@
+//package day44;
+//
+//public class ShareBank {
+//	public static void main(String[] args) {
+//		ATM atm = new ATM();
+//		Thread mom = new Thread(atm,"엄마");
+//		Thread son = new Thread(atm,"아들");
+//		
+//		mom.start();
+//		son.start();
+//		
+//	}
+//}
+// class ATM implements Runnable{
+//	int money = 10000;
+//	public void run() {
+//				for (int i = 0; i < 5; i++) {
+//					withdraw(1000);
+//					printInfo();
+//					try {
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {	}	}	}
+//	void withdraw(int money) {
+//		Thread.yield();
+//		this.money -= money;
+//		System.out.println(Thread.currentThread().getName() + "이(가) " + money + "원 출금");
+//	}
+//	void printInfo() {
+//		System.out.println("현재 잔액" + money + "원");
+//	}}
+//
+//
